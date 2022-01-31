0361aac9b56dae2c81a99de15f9d6870c76c1f64
arosario87 <arosario87@live.com>
2022-01-31 02:05:23 -0500

    Initial Commit

---
+++ TwitterCloneApp/TwitterCloneAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneAppApp.swift
+//  TwitterCloneApp
+//
+//  Created by Daniela Rosario on 1/31/22.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
