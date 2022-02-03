f7ccd146e7797ff7ccbcf47ab61c3ecc7dcebc49
arosario87 <arosario87@live.com>
2022-02-03 01:22:36 -0500

    SwiftUI Twitter Clone

---
+++ TwitterCloneApp/Utils/ImagePicker.swift
@@ -0,0 +1,49 @@
+//
+//  ProfilePhotoViewModel.swift
+//  TwitterCloneApp
+//
+//  Created by Angel Rosario on 2/1/22.
+//
+
+import SwiftUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+    
+    
+    
+    
+    @Binding var selectedImage : UIImage?
+    
+    
+    @Environment(\.dismiss) var dismiss
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(self)
+    }
+    
+    func makeUIViewController(context: Context) -> some UIViewController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        return picker
+    }
+    
+    
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        
+    }
+    
+}
+
+class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    let parent : ImagePicker
+    
+    init(_ parent: ImagePicker) {
+        self.parent = parent
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.originalImage] as? UIImage else { return }
+        self.parent.selectedImage = image
+        self.parent.dismiss.callAsFunction()
+    }
+    
+}
